e08eceb9fcbea1dce7918029bc9971bfab5efadf
OfekKadir <ofekkadir9@gmail.com>
2025-11-05 14:39:37 +0200

    fin4

--- CalcPi.java
+++ CalcPi.java
@@ -2,12 +2,12 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
-		System.out.println ("pi according to java: " + Math.PI);
+		System.out.println ("pi according to Java: " + Math.PI);
 		int numOfTerms = Integer.parseInt(args[0]);
 		double terms = 0;
-		int mechane = 3;
+		double mechane = 3;
 		double one = -1;
-		for (int i = 1; i < numOfTerms; i++) {
+		for (int i = 1; i < numOfTerms ; i++) {
 			terms = terms + (one / mechane);
 			one = -one;
 			mechane = mechane + 2;
