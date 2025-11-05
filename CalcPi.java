18bbbdac06ce41c761a77e6b39caddf320b27089
OfekKadir <ofekkadir9@gmail.com>
2025-11-05 15:08:01 +0200

    fin4

--- CalcPi.java
+++ CalcPi.java
@@ -4,17 +4,17 @@ public class CalcPi {
 	    // Replace this comment with your code
 		System.out.println ("pi according to Java: " + Math.PI);
 		int numOfTerms = Integer.parseInt(args[0]);
-		double terms = 0;
-		double mechane = 3;
-		double one = -1;
-		for (int i = 1; i < numOfTerms ; i++) {
+		double terms = 0.0;
+		double mechane = 1.0;
+		double one = 1.0;
+		for (int i = 0; i < numOfTerms ; i++) {
 			terms = terms + (one / mechane);
 			one = -one;
-			mechane = mechane + 2;
+			mechane = mechane + 2.0;
 		}
 
-		double piDivBy4 = 1 + terms; 
-		double pi = 4 * (piDivBy4);
+		double piDivBy4 = terms; 
+		double pi = 4.0 * (piDivBy4);
 		System.out.println ("pi, approximated:     " + pi);
 	}
 }
